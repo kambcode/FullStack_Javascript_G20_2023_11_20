7db2844f4309c067a836fd0dd6121e2006f5b59e
Jose Trespalacios Bedoya <josetrespalaciosbedoya@gmail.com>
2024-05-16 20:30:20 -0500

    Loops

---
+++ MODULO_5/C2/workclass/loops.js
@@ -0,0 +1,15 @@
+const estudiantes = [
+    "Jose Trespalacios",
+    "David Lopez", 
+    "Janeth Smith", 
+    "María Norenia"
+];
+
+for(let indice = 0; indice < estudiantes.length; indice++){
+    // Este sería el espacio de ejecución del for.
+    let moduleKC = "Backend";
+    console.log('El nombre del estudiante es: '+ estudiantes[indice]);
+    console.log('Y soy del módulo: '+ moduleKC)
+}
+
+console.log(estudiantes);
