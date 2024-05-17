5c5d5bd341590bd7d648ea07219145ec719ecd6b
Jose Trespalacios Bedoya <josetrespalaciosbedoya@gmail.com>
2024-05-16 20:40:03 -0500

    Arrays

---
+++ MODULO_5/C2/workclass/arrays.js
@@ -0,0 +1,13 @@
+// Esta no es la manera adecuada, para yo plantear una lista
+let estudiantes = "Jose, David, Janeht";
+
+// Esta si es la manera adecuada según el contexto de arrays
+const arrayBasico = [1,2,3,4,5,6,7,8,9];
+const arrayEstudiantes = ["Jose", "David", "Janeht"];
+
+console.log(arrayBasico);
+console.log(arrayEstudiantes);
+
+let frutas = ["Manzana", "Pera", "Banana", "Naranja"];
+
+console.log(frutas);
