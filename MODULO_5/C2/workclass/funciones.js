7840e89a7acbc5a467b2febcdaaffbab764720e0
Jose Trespalacios Bedoya <josetrespalaciosbedoya@gmail.com>
2024-05-16 19:53:59 -0500

    Base JS

---
+++ MODULO_5/C2/workclass/funciones.js
@@ -0,0 +1,20 @@
+function sumaDosNumeros(a, b) {
+    // Este es el espacio de ejecución de mi función
+    console.log(a+b)   
+}
+
+let suma = function () {
+    var num1 = 30;
+    var num2 = 58;
+
+    console.log(num1+num2)
+}
+
+// // En este punto, es donde se me ejecuta la función
+suma();
+
+function saludar () {
+    console.log('Hola, este es mi saludo.');
+}
+
+saludar();
