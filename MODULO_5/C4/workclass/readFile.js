1f2db16fcc53698d78aebef6e78c7412a58914ca
Jose Trespalacios Bedoya <josetrespalaciosbedoya@gmail.com>
2024-05-24 19:40:04 -0500

    Deleted base files

--- MODULO_5/C4/workclass/readFile.js
+++
@@ -1,5 +0,0 @@
-import fs from "fs";
-
-fs.readFile("./data.txt", (err, data) => {
-    console.log(data);
-});
--- MODULO_5/C4/workclass/readStreamNonFlowing.js
+++
@@ -1,8 +0,0 @@
-import { createReadStream } from "fs";
-
-var readStream = createReadStream("./data.txt");
-
-setTimeout(() => {
-    const data = readStream.read(10);
-    console.log(data);
-}, 10);
